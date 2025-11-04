95b26d77ac93fcf4e578f3e6068382adbb4af8c3
alaabadr-lab <alaamahmoudbadr5@gmail.com>
2025-11-04 15:19:14 +0200

    add to cart task

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 import Pages.CartScreen;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
